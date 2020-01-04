fcdee90fbec1f923343ed0d5ef8ae03b0f1418ca
Pradnya Satpute <pradnyasatpute23@gmail.com>
2020-01-05 03:07:55 +0530

    creating user with user_name and email field

--- user_app/models.py
+++ user_app/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
+
 class User(AbstractBaseUser):
-    pass
+    phone_number = models.CharField(max_length=10, unique=True)
+    email_id = models.EmailField(max_length=40, unique=True)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=200)
+    date_of_birth = models.DateField()
+    city = models.CharField(max_length=100)
+
+    USERNAME_FIELD = 'phone_number'
+    EMAIL_FIELD = 'email_id'
+    # for createsuperuser command will prompt for following
+    # REQUIRED_FIELDS = []
